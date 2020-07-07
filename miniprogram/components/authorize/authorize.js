b900e9e1367cc4235af56b1941e85b8c7b8dcc0e
陈蜜 <chenmi@sce-re.com>
2020-07-07 15:34:49 +0800

    抽取公共组件

--- miniprogram/components/authorize/authorize.js
+++ miniprogram/components/authorize/authorize.js
@@ -30,12 +30,11 @@ Component({
             //用户按了拒绝按钮
             wx.showModal({
                 title:'警告',
-                content:'您点击了拒绝授权，将无法进入小程序，请授权之后再进入!!!',
+                content:'您点击了拒绝授权，将无法使用小程序部分功能，请授权后继续',
                 showCancel:false,
                 confirmText:'返回授权',
                 success:function(res){
                     if (res.confirm) {
-                        console.log('用户点击了“返回授权”')
                     }
                 }
             })
---
+++ miniprogram/components/empty/index.js
@@ -0,0 +1,26 @@
+// components/empty/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    title: {
+      type: String,
+      value: '暂无数据😂'
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
